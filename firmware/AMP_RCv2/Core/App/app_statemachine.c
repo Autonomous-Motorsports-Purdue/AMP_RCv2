9cb39e6bcbf42171a4f68548eadccd08889c3841
Grant Yoder <70408179+gyoder@users.noreply.github.com>
2024-09-27 18:30:38 -0400

    Started on screen - needs hw testing

--- firmware/AMP_RCv2/Core/App/app_statemachine.c
+++ firmware/AMP_RCv2/Core/App/app_statemachine.c
@@ -1,7 +1,10 @@
 #include "spi.h"
 #include "usart.h"
+#include "math.h"
 
 #include "LoRa.h"
+
+#include "ssd1306.h" //https://github.com/afiskon/stm32-ssd1306
 
 #include "app_statemachine.h"
 
@@ -26,6 +29,8 @@ State_T current_state;		// variable holding current controller state
 uint32_t ticks_in_state;	// variable holding number of ticks in current state
 uint32_t temp_data;			// to hold miscellaneous data within a state
 
+uint16_t lora_debug;		// variable holding LoRa debug data
+
 uint8_t thrust;			// variable holding current thrust value
 uint8_t steering;		// variable holding current steering value
 char buttons;			// variable holding activated buttons
@@ -39,6 +44,12 @@ void App_StateMachine_Init()
 	thrust = 0;
 	steering = 128;
 	buttons = 0;
+
+
+	// initialize OLED
+	ssd1306_Init();
+	ssd1306_Fill(SSD1306_COLOR_BLACK);
+
 	// initialize LoRa
 	LoRa lora;
 	lora = newLoRa();
@@ -54,7 +65,7 @@ void App_StateMachine_Init()
 	lora.frequency = 915;
 
 	LoRa_reset(&lora);
-	uint16_t lora_debug = LoRa_init(&lora);
+	lora_debug = LoRa_init(&lora);
 	if (lora_debug == LORA_OK)
 	{
 		HAL_UART_Transmit(&huart2, (unsigned char *) "LoRa OK\r\n", 9, 10);
@@ -63,6 +74,11 @@ void App_StateMachine_Init()
 	{
 		HAL_UART_Transmit(&huart2, (unsigned char *) "LoRa FAILED\r\n", 13, 10);
 	}
+
+	// initialize OLED
+	
+
+
 	// set current state to idle
 	App_StateMachine_ChangeState(STATE_IDLE);
 }
@@ -72,6 +88,24 @@ void App_StateMachine_Tick()
 {
 	// statements to be called regardless of state
 	ticks_in_state += 1;
+
+	ssd1306_Fill(SSD1306_COLOR_BLACK); // clear OLED display
+	ssd1306_SetCursor(0, 0); // set cursor to top left corner
+	if (lora_debug == LORA_OK)
+	{
+		ssd1306_WriteString("LoRa OK", Font_11x18, SSD1306_COLOR_WHITE);
+	}
+	else
+	{
+		ssd1306_WriteString("LoRa FAILED", Font_11x18, SSD1306_COLOR_WHITE);
+	}
+
+	sssd1306_SetCursor(0, 20);
+	ssd1306_WriteString("State: ", Font_11x18, SSD1306_COLOR_WHITE);
+
+
+
+
 	// TODO need lora driver send function here
 	// run state-specific code
 	switch (current_state)
@@ -85,17 +119,25 @@ void App_StateMachine_Tick()
 		case (STATE_SLOW):
 		{
 			// TODO
+			ssd1306(SetCursor(0, 40));
+			ssd1306_WriteString("SLOW", Font_11x18, SSD1306_COLOR_WHITE);
+			Draw_Speedometer();
 			break;
 		}
 
 		case (STATE_FAST):
 		{
 			// TODO
+			ssd1306(SetCursor(0, 40));
+			ssd1306_WriteString("FAST", Font_11x18, SSD1306_COLOR_WHITE);
+			Draw_Speedometer();
 			break;
 		}
 
 		case (STATE_EBRAKE):
 		{
+			ssd1306_SetCursor(0, 40);
+			ssd1306_WriteString("EBRAKE", Font_11x18, SSD1306_COLOR_WHITE);
 			if (ticks_in_state > (EBRAKE_MIN_SEC * TICKS_PER_SEC))
 			{
 				// TODO only allow leaving after set amount of time
@@ -106,9 +148,15 @@ void App_StateMachine_Tick()
 		case (STATE_ERROR):
 		{
 			// TODO
+			ssd1306(SetCursor(0, 40));
+			ssd1306_WriteString("ERROR", Font_11x18, SSD1306_COLOR_WHITE);
 			break;
 		}
 	}
+
+
+	// update oled display
+	ssd1306_UpdateScreen();
 }
 
 void App_StateMachine_ChangeState(State_T new_state)
@@ -118,4 +166,46 @@ void App_StateMachine_ChangeState(State_T new_state)
 	temp_data = 0;
 	// change state
 	current_state = new_state;
+}
+
+
+void Draw_Speedometer()
+{
+	// draw circle outline
+	ssd1306_DrawArc(64, 32, 30, 45, 315, SSD1306_COLOR_WHITE);
+	ssd1306_DrawArc(64, 32, 20, 45, 315, SSD1306_COLOR_WHITE);
+	ssd1306_Line(64, 12, 64, 52, SSD1306_COLOR_WHITE);
+	ssd1306_Line(34, 32, 94, 32, SSD1306_COLOR_WHITE);
+
+	// draw speed indicator
+	for (int i = 20; i < 30; i++)
+	{
+		ssd1306_DrawArc(64, 32, i, 45, 45 + abs(thrust/SPEED_FWD_FAST * 270), SSD1306_COLOR_WHITE);
+	}
+
+	// draw speed number in middle
+	ssd1306_SetCursor(54, 24);
+	char speed[2];
+	sprintf(speed, "%d", thrust);
+	ssd1306_WriteString(speed, Font_11x18, SSD1306_COLOR_WHITE);
+
+
+
+}
+
+void Draw_Steering() 
+{
+	// draw hollow rectangle along bottom
+	ssd1306_DrawRectangle(4, 48, 124, 60, SSD1306_COLOR_WHITE);
+
+	// draw filled rectangle for steering starting in the center and going to where the steering is pointing
+	ssd1306_FillRectangle(64, 48, 64 + (steering), 60, SSD1306_COLOR_WHITE); // TODO: check what the steering variable is holding
+
+	// write steering text on the top right of the rectangle
+	ssd1306_SetCursor(96, 100);
+	char steer[3];
+	sprintf(steer, "%d", steering);
+	ssd1306_WriteString(steer, Font_11x18, SSD1306_COLOR_WHITE);
+
+
 }
